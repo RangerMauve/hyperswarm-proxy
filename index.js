82f2bfa4928f4cbdb93600884bb76e5f9244bc64
RangerMauve <ranger@mauve.moe>
2019-08-12 16:50:03 -0400

    Initial progress

---
+++ index.js
@@ -0,0 +1,9 @@
+var Duplex = require('readable-stream').Duplex
+var lps = require('length-prefixed-stream')
+var messages = require('./messages')
+
+module.exports = class HyperswarmProxyStream extends Duplex {
+  constructor(stream) {
+
+  }
+}
